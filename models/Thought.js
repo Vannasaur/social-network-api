bc7e34e6d1b4209984993cfbacf8df1c96a4eb65
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-10-16 18:29:07 -0700

    added content to models, safe revert point

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const { format_date } = require('../utils/helpers');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: [280, 'Uh-oh, too many characters!'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAt => format_date (createdAt)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// virtual property 'reactionCount' that gets the amount of friends per user
+userSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+// initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// virtual property 'friendCount' that gets the amount of friends per user
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+// initialize User model
+const User = model('user', userSchema);
+
+module.exports = User;
