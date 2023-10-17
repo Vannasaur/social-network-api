30e84d1e2a90d5984c15a3e7b07f393381557482
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-10-16 18:57:00 -0700

    added content to routes, safe revert point

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userControllers');
+
+// routes for /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// routes for api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// routes for api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
